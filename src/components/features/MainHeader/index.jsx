cf25cb7a4da23ed1431fd75997b63494e805532b
sadpandaemoji <sadpandaemoji@gmail.com>
2023-04-21 15:47:53 -0400

    add burger functionality at different resolutions

--- src/components/features/MainHeader/index.jsx
+++ src/components/features/MainHeader/index.jsx
@@ -1,49 +1,98 @@
 import Link from "next/link";
 import Image from "next/image";
 import classes from "./styles.module.css";
+import { useState } from "react";
 
 function MainHeader() {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  function handleMenuOpen() {
+    setIsMenuActive(true);
+  }
+  function handleMenuClose() {
+    setIsMenuActive(false);
+  }
+
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>
-        <Link href="/">
+      <Link href="/">
+        <Image
+          src="/assets/shared/logo.svg"
+          alt="logo"
+          width={40}
+          height={40}
+        />
+      </Link>
+
+      {isMenuActive ? (
+        <nav className={classes.navigationMobile}>
+          <button className={classes.button} onClick={handleMenuClose}>
+            <Image
+              src="/assets/shared/icon-close.svg"
+              alt="close-icon"
+              width={20}
+              height={20}
+            />
+          </button>
+          <ul className={classes.linksList}>
+            <li>
+              <Link className={classes.link} href="/">
+                <span className={classes.linkIndex}>00</span> HOME
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.link} href="/destination">
+                <span className={classes.linkIndex}>01</span> DESTINATION
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.link} href="/crew">
+                <span className={classes.linkIndex}>02</span> CREW
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.link} href="/technology">
+                <span className={classes.linkIndex}>03</span> TECHNOLOGY
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <button
+          className={`${classes.button} ${classes.menu}`}
+          onClick={handleMenuOpen}
+        >
           <Image
-            src="/assets/shared/logo.svg"
-            alt="logo"
-            width={40}
-            height={40}
+            src="/assets/shared/icon-hamburger.svg"
+            alt="menu-icon"
+            width={24}
+            height={21}
           />
-        </Link>
-      </div>
+        </button>
+      )}
       <nav className={classes.navigation}>
         <ul className={classes.linksList}>
           <li>
             <Link className={classes.link} href="/">
-              Home
+              00 HOME
             </Link>
           </li>
           <li>
             <Link className={classes.link} href="/destination">
-              Destination
+              01 DESTINATION
             </Link>
           </li>
           <li>
             <Link className={classes.link} href="/crew">
-              Crew
+              02 CREW
             </Link>
           </li>
           <li>
             <Link className={classes.link} href="/technology">
-              Technology
+              03 TECHNOLOGY
             </Link>
           </li>
         </ul>
-        <Image
-          src="/assets/shared/icon-hamburger.svg"
-          alt="menu-icon"
-          width={24}
-          height={21}
-        />
       </nav>
     </header>
   );
