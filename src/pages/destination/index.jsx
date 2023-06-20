7f20815f6a0b95ee02a7657d8c8147543ffe1d12
larr <sadpandaemoji@gmail.com>
2023-06-20 01:02:49 -0400

    add desktop breakout styling, add divs for flexbox layouts, add div for dynamic image sizing

--- src/pages/destination/index.jsx
+++ src/pages/destination/index.jsx
@@ -25,57 +25,64 @@ function Destination() {
       </Head>
       <main className={classes.content}>
         <article {...handlers} className={classes.container2}>
-          <h2 className={classes.intro}>
-            <span className={classes.dirIndex}>01</span>PICK YOUR DESTINATION
-          </h2>
-          <Image
-            className={classes.imageContainer}
-            src={data.destinations[destinationIndex].images.png.substring(1)}
-            alt={data.destinations[destinationIndex].name}
-            width={170}
-            height={170}
-          />
-          <div className={classes.navbarDestination}>
-            {data.destinations.map((entry, index) => (
-              <>
-                <button
-                  key={index}
-                  className={`${
-                    index == destinationIndex
-                      ? classes.btnNavActive
-                      : classes.btnNav
-                  }`}
-                  onClick={() => setDestinationIndex(index)}
-                >
-                  {entry.name.toUpperCase()}
-                  {index == destinationIndex ? (
-                    <div className={classes.btnNavActiveBar}></div>
-                  ) : null}
-                </button>
-              </>
-            ))}
+          <div className={classes.introContainer}>
+            <h2 className={classes.intro}>
+              <span className={classes.dirIndex}>01</span>PICK YOUR DESTINATION
+            </h2>
+            <div className={classes.imageContainer}>
+              <Image
+                className={classes.image}
+                src={data.destinations[destinationIndex].images.png.substring(
+                  1
+                )}
+                alt={data.destinations[destinationIndex].name}
+                fill
+              />
+            </div>
           </div>
-          <div className={classes.textContainer}>
-            <h1 className={classes.title}>
-              {data.destinations[destinationIndex].name.toUpperCase()}
-            </h1>
-            <p className={classes.text}>
-              {data.destinations[destinationIndex].description}
-            </p>
-          </div>
-          <hr className={classes.hRule} />
-          <div className={classes.detailsContainer}>
-            <div className={classes.detailsPair}>
-              <p className={classes.detailLabel}>AVG. DISTANCE</p>
-              <p className={classes.details}>
-                {data.destinations[destinationIndex].distance.toUpperCase()}
+          <div className={classes.descriptionContainer}>
+            <div className={classes.navbarDestination}>
+              {data.destinations.map((entry, index) => (
+                <>
+                  <button
+                    key={index}
+                    className={`${
+                      index == destinationIndex
+                        ? classes.btnNavActive
+                        : classes.btnNav
+                    }`}
+                    onClick={() => setDestinationIndex(index)}
+                  >
+                    {entry.name.toUpperCase()}
+                    {index == destinationIndex ? (
+                      <div className={classes.btnNavActiveBar}></div>
+                    ) : null}
+                  </button>
+                </>
+              ))}
+            </div>
+            <div className={classes.textContainer}>
+              <h1 className={classes.title}>
+                {data.destinations[destinationIndex].name.toUpperCase()}
+              </h1>
+              <p className={classes.text}>
+                {data.destinations[destinationIndex].description}
               </p>
             </div>
-            <div className={classes.detailsPair}>
-              <p className={classes.detailLabel}>EST. TRAVEL TIME</p>
-              <p className={classes.details}>
-                {data.destinations[destinationIndex].travel.toUpperCase()}
-              </p>
+            <hr className={classes.hRule} />
+            <div className={classes.detailsContainer}>
+              <div className={classes.detailsPair}>
+                <p className={classes.detailLabel}>AVG. DISTANCE</p>
+                <p className={classes.details}>
+                  {data.destinations[destinationIndex].distance.toUpperCase()}
+                </p>
+              </div>
+              <div className={classes.detailsPair}>
+                <p className={classes.detailLabel}>EST. TRAVEL TIME</p>
+                <p className={classes.details}>
+                  {data.destinations[destinationIndex].travel.toUpperCase()}
+                </p>
+              </div>
             </div>
           </div>
         </article>
