9c75f8e2bad54f961a8f72f05d7a0a7449ed0efb
larr <sadpandaemoji@gmail.com>
2023-06-20 23:17:31 -0400

    add hover states for tech nav, revise color for crew nav, change display style to grid for destination

--- src/pages/destination/index.jsx
+++ src/pages/destination/index.jsx
@@ -25,64 +25,58 @@ function Destination() {
       </Head>
       <main className={classes.content}>
         <article {...handlers} className={classes.container2}>
-          <div className={classes.introContainer}>
-            <h2 className={classes.intro}>
-              <span className={classes.dirIndex}>01</span>PICK YOUR DESTINATION
-            </h2>
-            <div className={classes.imageContainer}>
-              <Image
-                className={classes.image}
-                src={data.destinations[destinationIndex].images.png.substring(
-                  1
-                )}
-                alt={data.destinations[destinationIndex].name}
-                fill
-              />
-            </div>
+          <h2 className={classes.intro}>
+            <span className={classes.dirIndex}>01</span>PICK YOUR DESTINATION
+          </h2>
+          <div className={classes.imageContainer}>
+            <Image
+              className={classes.image}
+              src={data.destinations[destinationIndex].images.png.substring(1)}
+              alt={data.destinations[destinationIndex].name}
+              fill
+            />
           </div>
-          <div className={classes.descriptionContainer}>
-            <div className={classes.navbarDestination}>
-              {data.destinations.map((entry, index) => (
-                <>
-                  <button
-                    key={index}
-                    className={`${
-                      index == destinationIndex
-                        ? classes.btnNavActive
-                        : classes.btnNav
-                    }`}
-                    onClick={() => setDestinationIndex(index)}
-                  >
-                    {entry.name.toUpperCase()}
-                    {index == destinationIndex ? (
-                      <div className={classes.btnNavActiveBar}></div>
-                    ) : null}
-                  </button>
-                </>
-              ))}
-            </div>
-            <div className={classes.textContainer}>
-              <h1 className={classes.title}>
-                {data.destinations[destinationIndex].name.toUpperCase()}
-              </h1>
-              <p className={classes.text}>
-                {data.destinations[destinationIndex].description}
+          <div className={classes.navbarDestination}>
+            {data.destinations.map((entry, index) => (
+              <>
+                <button
+                  key={index}
+                  className={`${
+                    index == destinationIndex
+                      ? classes.btnNavActive
+                      : classes.btnNav
+                  }`}
+                  onClick={() => setDestinationIndex(index)}
+                >
+                  {entry.name.toUpperCase()}
+                  {index == destinationIndex ? (
+                    <div className={classes.btnNavActiveBar}></div>
+                  ) : null}
+                </button>
+              </>
+            ))}
+          </div>
+          <div className={classes.textContainer}>
+            <h1 className={classes.title}>
+              {data.destinations[destinationIndex].name.toUpperCase()}
+            </h1>
+            <p className={classes.text}>
+              {data.destinations[destinationIndex].description}
+            </p>
+          </div>
+          <hr className={classes.hRule} />
+          <div className={classes.detailsContainer}>
+            <div className={classes.detailsPair}>
+              <p className={classes.detailLabel}>AVG. DISTANCE</p>
+              <p className={classes.details}>
+                {data.destinations[destinationIndex].distance.toUpperCase()}
               </p>
             </div>
-            <hr className={classes.hRule} />
-            <div className={classes.detailsContainer}>
-              <div className={classes.detailsPair}>
-                <p className={classes.detailLabel}>AVG. DISTANCE</p>
-                <p className={classes.details}>
-                  {data.destinations[destinationIndex].distance.toUpperCase()}
-                </p>
-              </div>
-              <div className={classes.detailsPair}>
-                <p className={classes.detailLabel}>EST. TRAVEL TIME</p>
-                <p className={classes.details}>
-                  {data.destinations[destinationIndex].travel.toUpperCase()}
-                </p>
-              </div>
+            <div className={classes.detailsPair}>
+              <p className={classes.detailLabel}>EST. TRAVEL TIME</p>
+              <p className={classes.details}>
+                {data.destinations[destinationIndex].travel.toUpperCase()}
+              </p>
             </div>
           </div>
         </article>
